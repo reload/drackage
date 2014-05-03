f93e9fbb20115c26e4e88d5390b0c7222f34f150
Thomas Fini Hansen <xen@xen.dk>
2014-05-03 12:04:51 +0200

    Added package display page.

--- modules/drackage/lib/Drupal/drackage/Controller/DrackageController.php
+++ modules/drackage/lib/Drupal/drackage/Controller/DrackageController.php
@@ -10,6 +10,8 @@ namespace Drupal\drackage\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\drackage\Form\SearchForm;
 use Drupal\Core\Http\Client;
+use GuzzleHttp\Exception\TransferException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Controller routines for drackage routes.
@@ -19,25 +21,65 @@ class DrackageController extends ControllerBase {
    * Shows the search form.
    */
   public function searchPage() {
+    $settings = array(
+      'drackage_packages' => $this->packagistJson('search.json?tags[]=drush'),
+    );
+    $build['#attached']['js'][] = array('type' => 'setting', 'data' => $settings);
+    $build['form'] = $this->formBuilder()->getForm(new SearchForm());
+
+    return $build;
+  }
+
+  /**
+   * The _title_callback for the drackage.package_page route.
+   */
+  public function packagePageTitle($vendor, $package) {
+    return $this->t('Package @vendor/@package', array('@vendor' => $vendor, '@package' => $package));
+  }
+
+  /**
+   * Shows information about a package.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+   *   Exception is thrown when we couldn't fetch any information about the
+   *   given package.
+   */
+  public function packagePage($vendor, $package) {
+    $package_info = $this->packagistJson('packages/' . $vendor . '/' . $package . '.json');
+
+    if (empty($package_info)) {
+      throw new NotFoundHttpException();
+    }
+
+    $build['package'] = array(
+      '#type' => 'markup',
+      '#markup' => check_plain(print_r($package_info, true)),
+    );
+    return $build;
+  }
+
+  /**
+   * Make a request to Packagist and return the JSON response.
+   */
+  protected function packagistJson($path) {
     $http_client_options = array(
       'headers' => array(
         'User-Agent' => 'Drupal (+http://drupal.org/)',
       ),
     );
     $http_client = new Client($http_client_options);
-    $response = $http_client->get('https://packagist.org/search.json?tags[]=drush');
 
-    $packages = array();
-    if ($response->getStatusCode() == 200) {
-      $packages = $response->json();
-      $settings = array(
-        'drackage_packages' => $packages,
-      );
-      $build['#attached']['js'][] = array('type' => 'setting', 'data' => $settings);
+    $json = array();
+    try {
+      $response = $http_client->get('https://packagist.org/' . $path);
+
+      if ($response->getStatusCode() == 200) {
+        $json = $response->json();
+      }
     }
-
-    $build['form'] = $this->formBuilder()->getForm(new SearchForm());
-
-    return $build;
+    catch (TransferException $e) {
+      // Quietly swallow all Guzzle exceptions.
+    }
+    return $json;
   }
 }
